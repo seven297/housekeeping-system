19e20efe2e315ba8e42dd69942ee68333e04ca93
seven297 <924422304@qq.com>
2023-04-06 10:08:20 +0800

    feat: 添加导航栏路由跳转

--- react-admin-web/src/components/layout/Navigation.tsx
+++ react-admin-web/src/components/layout/Navigation.tsx
@@ -1,40 +1,47 @@
 import { DomainVerification, Groups, ListAlt, ThumbUp, Work } from "@mui/icons-material"
 import { ListItemIcon, ListItemText, MenuItem, MenuList } from "@mui/material"
+import { useNavigate } from "react-router-dom"
 
 function Navigation() {
+  const navigate = useNavigate()
+
+  const navTo = (path: string) => {
+    navigate(path)
+  }
+
   return (
     // TODO 动态关联routes
     <MenuList>
       <MenuItem>后台管理系统</MenuItem>
-      <MenuItem>
+      <MenuItem onClick={() => navTo('project')}>
         <ListItemIcon>
           <DomainVerification fontSize="small" />
         </ListItemIcon>
         <ListItemText>家政项目管理</ListItemText>
       </MenuItem>
-      <MenuItem>
+      <MenuItem onClick={() => navTo('worker')}>
         <ListItemIcon>
           <Work fontSize="small" />
         </ListItemIcon>
-        <ListItemText>家政人员管理</ListItemText>
+        <ListItemText>家政人员管理*</ListItemText>
       </MenuItem>
-      <MenuItem>
+      <MenuItem onClick={() => navTo('customer')}>
         <ListItemIcon>
           <Groups fontSize="small" />
         </ListItemIcon>
-        <ListItemText>客户管理</ListItemText>
+        <ListItemText>客户管理*</ListItemText>
       </MenuItem>
-      <MenuItem>
+      <MenuItem onClick={() => navTo('order')}>
         <ListItemIcon>
           <ListAlt fontSize="small" />
         </ListItemIcon>
         <ListItemText>订单管理</ListItemText>
       </MenuItem>
-      <MenuItem>
+      <MenuItem onClick={() => navTo('comment')}>
         <ListItemIcon>
           <ThumbUp fontSize="small" />
         </ListItemIcon>
-        <ListItemText>客户评价管理</ListItemText>
+        <ListItemText>客户评价管理*</ListItemText>
       </MenuItem>
     </MenuList>
   )
