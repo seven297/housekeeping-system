4a647b019af013e949ec447939147cf305f4f236
seven297 <924422304@qq.com>
2023-04-20 14:57:12 +0800

    feat: 增加sql语句

--- express-web-server/src/sqls/auth.ts
+++ express-web-server/src/sqls/auth.ts
@@ -1,1 +1,12 @@
 /** 权限数据库操作语句 */
+export default class AuthSql {
+  // 查询全部数据
+  public static query() {
+    return 'SELECT * FROM user'
+  }
+
+  // 查询指定id数据
+  public static queryById(id: number) {
+    return `SELECT * FROM user WHERE id = ${id}`
+  }
+}
